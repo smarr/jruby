1bc067333ff659f90548f6010c59f13cd775ea83
Christian Meier <m.kristian@web.de>
2014-10-27 11:30:07 +0000

    added osgi test without inlining the dependencies

--- maven/jruby/pom.rb
+++ maven/jruby/pom.rb
@@ -36,12 +36,19 @@ project 'JRuby Main Maven Artifact' do
 
   execute 'setup other osgi frameworks', :phase => 'pre-integration-test' do |ctx|
     source = File.join( ctx.basedir.to_pathname, 'src', 'templates', 'osgi_all_inclusive' )
-     [ 'knoplerfish', 'equinox-3.6', 'equinox-3.7', 'felix-3.2', 'felix-4.4'].each do |m|
-      target = File.join( ctx.basedir.to_pathname, 'src', 'it', 'osgi_all_inclusive_' + m )
-      FileUtils.rm_rf( target )
-      FileUtils.cp_r( source, target )
-      File.open( File.join( target, 'invoker.properties' ), 'w' ) do |f|
-        f.puts 'invoker.profiles = ' + m
+    [ true, false ].each do |inline|
+      prefix = 'first_'
+      [ 'knoplerfish', 'equinox-3.6', 'equinox-3.7', 'felix-3.2', 'felix-4.4'].each do |m|
+        target = File.join( ctx.basedir.to_pathname, 'src', 'it', "osgi_#{prefix}all_inclusive_#{inline ? 'inline' : 'jars' }_#{m}" )
+        prefix = ''
+        FileUtils.rm_rf( target )
+        FileUtils.cp_r( source, target )
+        File.open( File.join( target, 'invoker.properties' ), 'w' ) do |f|
+          f.puts "invoker.profiles = #{m}"
+        end
+        File.open( File.join( target, 'test.properties' ), 'w' ) do |f|
+          f.puts "inline = #{inline}"
+        end
       end
     end
   end
